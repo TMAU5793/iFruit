ff83a888873435b5345ebc907bc3a02294318ff6
wriy9o <thank@grasp.asia>
2020-09-08 18:52:15 +0700

    payment

--- application/views/payment.php
+++ application/views/payment.php
@@ -13,7 +13,7 @@
 			<input type="hidden" name="hd_invoice" value="<?php echo $order->invoice_no; ?>">
 			<input type="hidden" name="hd_nettotal" value="<?php echo $order->nettotal; ?>">
 			<input type="hidden" name="hd_method" value="internet_banking">
-			<button type="submit" id="iBangkingButton">ชำระเงินผ่าน Internet Bangking</button>
+			<button type="button" id="iBangkingButton">ชำระเงินผ่าน Internet Bangking</button>
 		</form>
 		<form id="counterForm" method="POST" action="<?php echo base_url('Cart/confirmation'); ?>">
 			<input type="hidden" name="omiseToken">
@@ -21,7 +21,15 @@
 			<input type="hidden" name="hd_invoice" value="<?php echo $order->invoice_no; ?>">
 			<input type="hidden" name="hd_nettotal" value="<?php echo $order->nettotal; ?>">
 			<input type="hidden" name="hd_method" value="bill_payment_tesco_lotus">
-			<button type="submit" id="counterButton">ชำระเงินผ่าน เคาน์เตอร์ (เฉพาะเทสโก้โลตัส)</button>
+			<button type="button" id="counterButton">ชำระเงินผ่าน เคาน์เตอร์ (เฉพาะเทสโก้โลตัส)</button>
+		</form>
+		<form id="truemoneyForm" method="POST" action="<?php echo base_url('Cart/confirmation'); ?>">
+			<input type="hidden" name="omiseToken">
+			<input type="hidden" name="omiseSource">
+			<input type="hidden" name="hd_invoice" value="<?php echo $order->invoice_no; ?>">
+			<input type="hidden" name="hd_nettotal" value="<?php echo $order->nettotal; ?>">
+			<input type="hidden" name="hd_method" value="truemoney">
+			<button type="button" id="truemoneyButton">ชำระเงินผ่าน ทรูมันนี่</button>
 		</form>
 	</div>
 </section>
@@ -30,6 +38,7 @@
 	OmiseCard.configure({
 		publicKey: '<?php echo OMISE_PUBLIC_KEY ?>', // เข้าสู่ระบบของ Omise และไปที่ API -> Keys คีย์นี้จะเป็นตัวบอกว่า ชำระเข้าบัญชีของใคร
 	});
+	Omise.setPublicKey('<?php echo OMISE_PUBLIC_KEY ?>');
 	var nettotal = '<?php echo str_replace('.','',$order->nettotal) ?>';
 
 	var creditButton = document.querySelector("#creditButton");
@@ -56,35 +65,34 @@
 	var iBangkingButton = document.querySelector("#iBangkingButton");
 	var iBangkingForm = document.querySelector("#iBangkingForm");	
 	iBangkingButton.addEventListener("click", (event) => {
-		event.preventDefault();
-		// OmiseCard.open({
-		// 	frameLabel:"iFruit Brand",
-		// 	image:'<?php echo base_url('assets/images/favicon.png') ?>',
-		// 	amount: nettotal,  // เช่น ถ้าชำระ 20 บาท ต้องใส่เป็น 2000;
-		// 	currency: "THB",// สกุลเงิน
-		// 	defaultPaymentMethod: "internet_banking", // ช่องทางการชำระเงิน ค่าเริ่มต้นเป็น credit_card
-		// 	onCreateTokenSuccess: (nonce) => {
-		// 		if (nonce.startsWith("tokn_")) {
-		// 			iBangkingForm.omiseToken.value = nonce;
-		// 		} else {
-		// 			iBangkingForm.omiseSource.value = nonce;
-		// 		};
-		// 		iBangkingForm.submit();
-		// 	}
-		// });
-		iBangkingForm.submit();
+		Omise.createSource('internet_banking', {
+			"amount": 400000,
+			"currency": "THB"
+		}, function(statusCode, response) {
+			console.log(response)
+		});
 	});
 
 	var counterButton = document.querySelector("#counterButton");
 	var counterForm = document.querySelector("#counterForm");	
 	counterButton.addEventListener("click", (event) => {
-		event.preventDefault();
-		OmiseCard.open({
-			frameLabel:"iFruit Brand",
-			image:'<?php echo base_url('assets/images/favicon.png') ?>',
-			amount: nettotal,  // เช่น ถ้าชำระ 20 บาท ต้องใส่เป็น 2000;
-			currency: "THB",// สกุลเงิน
-			defaultPaymentMethod: "bill_payment_tesco_lotus", // ช่องทางการชำระเงิน ค่าเริ่มต้นเป็น credit_card			
+		Omise.createSource('bill_payment_tesco_lotus', {
+			"amount": 400000,
+			"currency": "THB"
+		}, function(statusCode, response) {
+			console.log(response)
+		});
+	});
+
+	var truemoneyButton = document.querySelector("#truemoneyButton");
+	var truemoneyForm = document.querySelector("#truemoneyForm");	
+	truemoneyButton.addEventListener("click", (event) => {
+		Omise.createSource('truemoney', {
+			"amount": nettotal,
+			"currency": "THB",
+			"phone_number": "0812345678"
+		}, function(statusCode, response) {
+			console.log(response)
 		});
 	});
 </script>
